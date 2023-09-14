9515d7a71d111d91dab6b96c2f7e6221cf80c390
Heba Elayoty <31887807+helayoty@users.noreply.github.com>
2023-09-13 17:08:31 -0700

    feat: Add GitHub workflow for tests (#10)
    * Add unit tests Github workflow
    
    Signed-off-by: Heba Elayoty <hebaelayoty@gmail.com>
    
    * Add unit tests Github workflow
    
    Signed-off-by: Heba Elayoty <hebaelayoty@gmail.com>
    
    ---------
    
    Signed-off-by: Heba Elayoty <hebaelayoty@gmail.com>

--- Makefile-az.mk
+++ Makefile-az.mk
@@ -308,3 +308,12 @@ release-manifest:
 	git add ./Makefile ./charts/gpu-provisioner/Chart.yaml ./charts/gpu-provisioner/values.yaml ./charts/gpu-provisioner/README.md
 	git commit -s -m "release: update manifest and helm charts for ${VERSION}"
 
+## --------------------------------------
+## Tests
+## --------------------------------------
+
+.PHONY: unit-test
+unit-test: ## Run unit tests.
+	go test -v ./pkg/providers/instance ./pkg/cloudprovider  \
+	-race -coverprofile=coverage.txt -covermode=atomic fmt
+	go tool cover -func=coverage.txt
