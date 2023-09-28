a8c62d357b33dee381015e0d92011e2296d0b49c
Heba Elayoty <hebaelayoty@gmail.com>
2023-09-28 15:03:58 -0700

    Update SKU cache and pricing, remove node_state
    Signed-off-by: Heba Elayoty <hebaelayoty@gmail.com>

--- pkg/staticprovisioner/provisioner.go
+++ pkg/staticprovisioner/provisioner.go
@@ -1,20 +1,24 @@
 package staticprovisioner
 
 import (
+	"github.com/Azure/go-autorest/autorest/to"
 	"github.com/aws/karpenter-core/pkg/apis/v1alpha5"
+	v1 "k8s.io/api/core/v1"
 )
 
 var (
 	Sp = &v1alpha5.Provisioner{
 		Spec: v1alpha5.ProvisionerSpec{
-			//Requirements: []v1.NodeSelectorRequirement{
-			//	v1.NodeSelectorRequirement{
-			//		Key:      "karpenter.k8s.azure/sku-family",
-			//		Operator: "In",
-			//		Values:   []string{"N"},
-			//	},
-			//},
-
+			Consolidation: &v1alpha5.Consolidation{
+				Enabled: to.BoolPtr(false),
+			},
+			Taints: []v1.Taint{
+				{
+					Key:    "sku",
+					Value:  "gpu",
+					Effect: v1.TaintEffectNoSchedule,
+				},
+			},
 			ProviderRef: &v1alpha5.MachineTemplateRef{
 				Name: "default",
 			},
