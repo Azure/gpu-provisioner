93847b3f4fac93ad0f1b531d4053bee1543a3bb2
Heba <31887807+helayoty@users.noreply.github.com>
2025-03-04 14:16:14 -0800

    chore: Remove unused env var (#208)
    The `AZURE_NODE_RESOURCE_GROUP` was set as an environment variable as we
    forked the repo from Azure Karpenter. For gpu-provisioner, there is no
    need to pass this env variable via chart.
    
    This PR is to clean up all the uses of this env variable in the
    codebase.
    
    Signed-off-by: Heba Elayoty <heelayot@microsoft.com>

--- hack/deploy/configure-helm-values.sh
+++ hack/deploy/configure-helm-values.sh
@@ -19,13 +19,12 @@ AZURE_GPU_PROVISIONER_USER_ASSIGNED_IDENTITY_NAME=$3
 
 AKS_JSON=$(az aks show --name "$CLUSTER_NAME" --resource-group "$AZURE_RESOURCE_GROUP" -o json)
 AZURE_LOCATION=$(jq -r ".location" <<< "$AKS_JSON")
-AZURE_RESOURCE_GROUP_MC=$(jq -r ".nodeResourceGroup" <<< "$AKS_JSON")
 AZURE_TENANT_ID=$(az account show -o json |jq -r ".tenantId")
 AZURE_SUBSCRIPTION_ID=$(az account show -o json |jq -r ".id")
 
 GPU_PROVISIONER_USER_ASSIGNED_CLIENT_ID=$(az identity show --resource-group "${AZURE_RESOURCE_GROUP}" --name "${AZURE_GPU_PROVISIONER_USER_ASSIGNED_IDENTITY_NAME}" --query 'clientId' -otsv)
 
-export CLUSTER_NAME AZURE_LOCATION AZURE_RESOURCE_GROUP AZURE_RESOURCE_GROUP_MC GPU_PROVISIONER_USER_ASSIGNED_CLIENT_ID AZURE_TENANT_ID AZURE_SUBSCRIPTION_ID
+export CLUSTER_NAME AZURE_LOCATION AZURE_RESOURCE_GROUP GPU_PROVISIONER_USER_ASSIGNED_CLIENT_ID AZURE_TENANT_ID AZURE_SUBSCRIPTION_ID
 
 # get gpu-provisioner-values-template.yaml, if not already present (e.g. outside of repo context)
 if [ ! -f gpu-provisioner-values-template.yaml ]; then
